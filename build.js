28824bace25e09f7a3c2172c3583bfaa7193f6fd
Stefan Stoichev <stefan.stoichev@gmail.com>
2019-11-18 11:17:01 +0000

    wokring

--- build.js
+++ build.js
@@ -1,6 +1,6 @@
 var ghpages = require('gh-pages');
- 
-ghpages.publish('dist', function(err) {
-    if(err) console.log(err)
+
+ghpages.publish('dist', function (err) {
+    if (err) console.log(err)
     console.log('done')
 });
